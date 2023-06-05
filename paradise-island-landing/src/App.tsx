a3c9a752510bde280c44d47ef50f5a799f118d07
Jaga <jagadheesh6@gmail.com>
2023-06-05 21:05:22 +0530

    feat - Hero component

--- paradise-island-landing/src/App.tsx
+++ paradise-island-landing/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Topbar from './components/Topbar';
+import Hero from './components/Hero';
 
 function App() {
 	return (
 		<div>
 			<Topbar />
 			<Navbar />
+			<Hero />
 		</div>
 	);
 }
---
+++ paradise-island-landing/src/components/Hero.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Tree from '../assets/tree.jpg';
+
+const Hero = () => {
+	return (
+		<div>
+			<div className='flex flex-col justify-center items-center h-[88vh] bg-cover' style={{backgroundImage: `url(${Tree})`}}>
+				<h1 className='text-6xl font-normal py-[12px]'>Find your special trip</h1>
+				<h2 className='text-4xl font-extralight py-[12px]'>With Paradise Island</h2>
+				<p className='text-xl font-sans text-center max-w-4xl py-[12px]'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+				dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
